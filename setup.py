af136ef48502a6337a5bdc156a37bfa46414ec48
Vladimir <vmstarchenko@edu.hse.ru>
2021-09-11 18:01:56 +0300

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='jiml',
     packages=['jiml'],
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     description='Convert json to json using jinja2 and yaml',
     long_description=long_description,
